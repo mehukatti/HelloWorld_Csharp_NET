949f103b744dd1beb8884570598d42efa064c50c
mehukatti <28701834+mehukatti@users.noreply.github.com>
2025-01-22 20:22:59 +0200

    Hello world tutorial

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,7 @@
+﻿// Tutorial from dotnet.microsoft https://www.youtube.com/watch?v=uytNCXw9dME&list=PLdo4fOcmZ0oUwBEC2bnwPtHqbU8Vmh_tj&index=2&ab_channel=dotnet
+Console.WriteLine("What is your name?");
+var name = Console.ReadLine();
+var currentDate = DateTime.Now;
+Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}");
+Console.Write($"{Environment.NewLine}Press any key to exit...");
+Console.ReadKey(true);
